827c8910c916f39e092ecaa2d610f9170ae6f003
radhome <radhome@github.com>
2020-06-28 15:21:52 +0800

    modified:   Week03/NOTE.md

---
+++ Week03/[46]全排列.java
@@ -0,0 +1,44 @@
+//给定一个 没有重复 数字的序列，返回其所有可能的全排列。 
+//
+// 示例: 
+//
+// 输入: [1,2,3]
+//输出:
+//[
+//  [1,2,3],
+//  [1,3,2],
+//  [2,1,3],
+//  [2,3,1],
+//  [3,1,2],
+//  [3,2,1]
+//] 
+// Related Topics 回溯算法
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+    List<List<Integer>> res = new LinkedList<>();
+
+    public List<List<Integer>> permute(int[] nums) {
+        LinkedList<Integer> track = new LinkedList<>();
+        backtrack(nums, track);
+        return res;
+    }
+
+    private void backtrack(int[] nums, LinkedList<Integer> track) {
+        if (track.size() == nums.length) {
+            res.add(new LinkedList(track));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (track.contains(nums[i]))
+                continue;
+            track.add(nums[i]);
+            backtrack(nums, track);
+            track.removeLast();
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
